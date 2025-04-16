2ccc063ce6694a279e88904eaf453e313246298c
Andrey Zashchepin <razlivnoy@icloud.com>
2025-04-16 22:19:40 +0500

    Модель Пользователь

--- users/models.py
+++ users/models.py
@@ -1,5 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Номер телефона',
+                                    help_text='Необязательное поле. Введите ваш номер телефона.')
+    city = models.CharField(max_length=30, blank=True, null=True, verbose_name='Город')
+    avatar = models.ImageField(upload_to='avatar/', blank=True, null=True, verbose_name='Аватар',
+                               help_text='Загрузите изображение.')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email

bac870221c1bf848e927865afdbed58bdcf1732b
Andrey Zashchepin <razlivnoy@icloud.com>
2025-04-22 17:45:31 +0500

    Фильтрация для эндпоинта Платежи

--- config/settings.py
+++ config/settings.py
@@ -23,6 +23,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
+
     "users",
     "materials",
 ]
@@ -56,6 +58,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 
 DATABASES = {
     "default": {
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -12,6 +14,9 @@ class PaymentListAPIView(generics.ListAPIView):
 
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    ordering_fields = ('date',)
+    filterset_fields = ('paid_course', 'paid_lesson', 'method',)
 
 
 class UserViewSet(ViewSet):

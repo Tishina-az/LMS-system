f91caddbf6cce5b903f21a2400c85027eb8707ed
Andrey Zashchepin <razlivnoy@icloud.com>
2025-04-23 20:07:45 +0500

    Мелкие правки

--- materials/serializers.py
+++ materials/serializers.py
@@ -5,6 +5,7 @@ from materials.models import Course, Lesson
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    """Сериализатор урока."""
 
     class Meta:
         model = Lesson
@@ -12,17 +13,27 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseLessonSerializer(serializers.ModelSerializer):
+    """Упрощенный сериализатор урока для вложенного отображения в курсе."""
 
     class Meta:
         model = Lesson
-        fields = ('id', 'title', 'description', 'link',)
+        fields = (
+            "id",
+            "title",
+            "description",
+            "link",
+        )
+        read_only_fields = ("id",)
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор курса с дополнительными полями."""
+
     lessons_count = SerializerMethodField()
     lessons = CourseLessonSerializer(many=True)
 
     def get_lessons_count(self, course):
+        """Возвращает количество уроков в курсе."""
         return Lesson.objects.filter(course=course).count()
 
     class Meta:
@@ -34,3 +45,4 @@ class CourseSerializer(serializers.ModelSerializer):
             "lessons_count",
             "lessons",
         )
+        read_only_fields = ("id", "lessons_count", "lessons")

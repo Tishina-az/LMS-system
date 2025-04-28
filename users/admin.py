de837bf2748aa0b0ca5f762ea15ff92d6d58d7eb
Andrey Zashchepin <razlivnoy@icloud.com>
2025-04-28 18:49:17 +0500

    Команда создания группы Модератор

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админ-панель для управления CustomUser."""
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_staff', 'is_active',)
+    list_filter = ('is_active', 'is_staff',)
+    search_fields = ('username', 'email', 'first_name', 'last_name',)
